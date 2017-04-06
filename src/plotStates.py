e65881cd768b8b3837bbd1ba43f7255c55a5286b
Yu Lu <yulu@utexas.edu>
2017-04-06 11:01:22 -0500

    corrected plot save file error

--- src/plotStates.py
+++ src/plotStates.py
@@ -7,36 +7,31 @@ folder = '../dat/'
 if os.path.exists(folder):
     file1 = 'numInitState.csv'
     data2D = np.loadtxt(folder + file1,delimiter = ',')
-    
     p1 = plt.figure()
     timeX = np.linspace(0, len(data2D[:,0]) , len(data2D[:,0]))
-    plt.plot(timeX, data2D[:,0],'*', label = 'm=-1')
-    plt.plot(timeX, data2D[:,1],'*', label = 'm=-0')
-    plt.plot(timeX, data2D[:,2],'*', label = 'm=1')
+    plt.plot(timeX, data2D[:,0],'*', label = 'F=1,m=-1')
+    plt.plot(timeX, data2D[:,1],'*', label = 'F=1,m=-0')
+    plt.plot(timeX, data2D[:,2],'*', label = 'F=1,m=1')
     plt.title('Ground State F = 1 hyperfine states')
     plt.xlabel('number of emission cycles')
     plt.ylabel('Population')
     plt.legend()
     p1.savefig(folder + file1[:-4] + '.png', dpi=150)
     
-    
-    #print("--- Plot saved in :  " + folder + f[:-4] +'.png ---')
-    p1.savefig(folder + file1[:-4] + '.png', dpi=150)
-
+        
     file2 = 'numTargetState.csv'
     data2 = np.loadtxt(folder + file2,delimiter = ',')
-    
     p2 = plt.figure()
     timeX = np.linspace(0, len(data2[:,0]) , len(data2[:,0]))
-    plt.plot(timeX, data2[:,0],'*', label = 'm=-2')
-    plt.plot(timeX, data2[:,1],'*', label = 'm=-1')
-    plt.plot(timeX, data2[:,2],'*', label = 'm=0')
-    plt.plot(timeX, data2[:,3],'*', label = 'm=1')
-    plt.plot(timeX, data2[:,4],'*', label = 'm=2')
+    plt.plot(timeX, data2[:,0],'*', label = 'F=2,m=-2')
+    plt.plot(timeX, data2[:,1],'*', label = 'F=2,m=-1')
+    plt.plot(timeX, data2[:,2],'*', label = 'F=2,m=0')
+    plt.plot(timeX, data2[:,3],'*', label = 'F=2,m=1')
+    plt.plot(timeX, data2[:,4],'*', label = 'F=2,m=2')
     plt.title('Ground State F = 2 hyperfine states')
     plt.xlabel('number of emission cycles')
     plt.ylabel('Population')
     plt.legend()
-    p2.savefig(folder + file1[:-4] + '.png', dpi=150)
+    p2.savefig(folder + file2[:-4] + '.png', dpi=150)
     plt.show()
         
